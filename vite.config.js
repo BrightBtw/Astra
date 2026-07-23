6f555ba49803c1b5e2277d9e2e92ccf43483a564
Andrey Kisarin <diulsya96@gmail.com>
2026-07-24 00:42:49 +0500

    setup deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import path from "path";
 import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
+  base: "https://github.com/BrightBtw/Astra",
   plugins: [injectHTML()],
   resolve: {
     alias: {
